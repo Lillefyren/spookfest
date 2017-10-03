abfeb567b292d8f6b475734976f5899b9a150022
Casper_Ingemann_Staehr <spillecasper@hotmail.com>
2017-10-03 15:12:13 +0200

    spookwriteout

--- spookzone.cs
+++ spookzone.cs
@@ -1,6 +1,18 @@
+using system.collections;
+using Unity;
+using system.generic;
 
-void GetSpooks(int spooks){
+public class spookzone : Monobehaviour{
+
+public int spooks;
+
+void Start(){
+  InvokeRepeatable(1, 1, "GetSpooks()")
+}
+
+void GetSpooks(){
 if(spooks < bonezone.bones){
-spooks += 1
+spooks += 1;
+}
 }
 }
